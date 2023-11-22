60f46f38eccf162418b4cf27c5807bb9efe1116b
Zeljko <olah.zeljko@gmail.com>
2023-11-22 23:46:42 +0100

    #add-invoice-avatar-alt

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -86,6 +86,7 @@ export default async function InvoicesTable({
                   <td className="whitespace-nowrap py-3 pl-6 pr-3">
                     <div className="flex items-center gap-3">
                       <Image
+                        alt={'invoice image'}
                         src={invoice.image_url}
                         className="rounded-full"
                         width={28}
