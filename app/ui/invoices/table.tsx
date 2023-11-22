46e92ed004d44e6b4d56853a5051e74ac26db25b
Zeljko <olah.zeljko@gmail.com>
2023-11-22 23:44:10 +0100

    #add-invoice-avatar

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,6 +27,7 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
+                        alt={'invoice avatar'}
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
